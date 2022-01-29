94f7a8018fcb9bdec0d90ef8558db522380508a7
AnthoJaraBustamante <omarbush81@gmail.com>
2022-01-28 21:42:14 -0500

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-import 'package:pokedex_flutter/app/bindings/splash_binding.dart';
+import 'package:pokedex_flutter/app/bindings/home_binding.dart';
 import 'package:pokedex_flutter/app/routes/app_pages.dart';
 import 'package:pokedex_flutter/app/routes/routes.dart';
 import 'package:pokedex_flutter/app/ui/dependency_injection.dart';
-import 'package:pokedex_flutter/app/ui/pages/splash_page/splash_page.dart';
+import 'package:pokedex_flutter/app/ui/pages/home_page/home_page.dart';
 import 'package:resize/resize.dart';
 
 void main() {
@@ -25,8 +25,8 @@ void main() {
           debugShowCheckedModeBanner: false,
           defaultTransition: Transition.fade,
           getPages: AppPages.pages,
-          home: const SplashPage(),
-          initialBinding: SplashBinding(),
+          home: const HomePage(),
+          initialBinding: HomeBinding(),
           initialRoute: Routes.splash,
         );
       },
